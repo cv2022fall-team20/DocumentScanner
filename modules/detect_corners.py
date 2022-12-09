e14ca49b0cf2a3a640d14d8a3e6b6dedafe5ab19
JC <39209141+jhc13@users.noreply.github.com>
2022-12-10 03:21:01 +0900

    Sort and label the detected corners

--- modules/detect_corners.py
+++ modules/detect_corners.py
@@ -11,11 +11,32 @@ MAX_EPSILON_MULTIPLIER = 0.1
 MIN_AREA_PROPORTION = 0.1
 
 
+def sort_corner_coordinates(corners: tuple[tuple[int, int], tuple[int, int],
+                                           tuple[int, int], tuple[int, int]]) \
+        -> tuple[tuple[int, int], tuple[int, int],
+                 tuple[int, int], tuple[int, int]]:
+    """
+    Sort the coordinates of the detected corners in clockwise order starting
+    from the top-left corner.
+    """
+    # Calculate the center coordinates of the detected corners.
+    center_x = sum(corner[0] for corner in corners) / 4
+    center_y = sum(corner[1] for corner in corners) / 4
+    # Sort the corners by their angle relative to the center. The y-axis is
+    # inverted in the image coordinates.
+    corners = sorted(corners,
+                     key=lambda corner: np.arctan2(corner[1] - center_y,
+                                                   corner[0] - center_x))
+    return corners
+
+
 def detect_corners(image: np.ndarray) \
         -> tuple[tuple[int, int], tuple[int, int],
                  tuple[int, int], tuple[int, int]]:
     """
-    Detect the four corners of a document in an image.
+    Detect the four corners of a document in an image and return their
+    coordinates. The coordinates are sorted in clockwise order starting from
+    the top-left corner.
     """
     grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred_image = cv2.GaussianBlur(grayscale_image,
@@ -47,7 +68,8 @@ def detect_corners(image: np.ndarray) \
                     and cv2.contourArea(approximated_contour)
                     >= MIN_AREA_PROPORTION * image_area):
                 corners = tuple(map(tuple, approximated_contour.reshape(4, 2)))
-                return corners
+                sorted_corners = sort_corner_coordinates(corners)
+                return sorted_corners
         else:
             epsilon_multiplier += EPSILON_MULTIPLIER_INCREMENT
     else:
@@ -60,6 +82,16 @@ def main():
     # Draw the edges of the document.
     for i in range(4):
         cv2.line(image, corners[i], corners[(i + 1) % 4], (0, 0, 255), 2)
+    # Label the corners.
+    font = cv2.FONT_HERSHEY_DUPLEX
+    font_scale = 1
+    text_thickness = 2
+    text_size, _ = cv2.getTextSize('0', font, font_scale, text_thickness)
+    for i, corner in enumerate(corners):
+        text_origin = (corner[0] - text_size[0] // 2,
+                       corner[1] + text_size[1] // 2)
+        cv2.putText(image, str(i), text_origin, font, font_scale, (0, 255, 0),
+                    text_thickness, cv2.LINE_AA)
     cv2.imshow('Detected document', image)
     cv2.waitKey(0)
 
